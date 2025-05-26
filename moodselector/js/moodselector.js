34aa32f6c49e648b0a2f87cc138608908f406c1a
lily-diamond <brielle.laplante22@gmail.com>
2025-05-26 16:36:00 -0700

    Fixed redundancies in moodselector.js
    functions similiarly, code is just a bit neater, left previous code commented out, may delete for final presentation

--- moodselector/js/moodselector.js
+++ moodselector/js/moodselector.js
@@ -1,7 +1,10 @@
 //imports the global constant varibales
 import * as default_values from "../../js/site.js";
 
-
+// Some code here has been commented out and reimplemented so that it works with boolean array output
+//
+//
+//
 
 // Makes it so that when you click on the "Return to Title Screen" button, you go to that page
 $("#title-screen").click(function() {
@@ -9,105 +12,134 @@ $("#title-screen").click(function() {
 });
 
 // Initializing all the Boolean variables of the user's emotions
-// let happy = false;
-// let sad = false;
-// let frustrated = false;
-// let tired = false;
-// let studious = false;
-// let energetic = false;
-// let anxious = false;
-// let nauseous = false;
+/*
+let happy = false;
+let sad = false;
+let frustrated = false;
+let tired = false;
+let studious = false;
+let energetic = false;
+let anxious = false;
+let nauseous = false;
+*/
 
 // Initializing an array with each of the emotion Booleans so that they can be easily accessed as a group
 let emotionsArray = [false, false, false, false, false, false, false, false];
 
+//new function implemented to replace redundant code
+let inverse = function(index, id, color){
+  //console.log("clicked on " + id + " in state: " + emotionsArray[index])
+
+  //flips color
+  if(emotionsArray[index])
+  {
+    document.getElementById(id).style.backgroundColor="#4dabf7";
+  }
+  else
+  {
+    document.getElementById(id).style.backgroundColor=color;
+  }
+
+  //flips value of what was stored
+  emotionsArray[index] = !emotionsArray[index];
+  ;
+};
+
 // Creates an event listener that updates the emotion Booleans in the array based on user clicks, as well as the button's color
 $("#happy-button").click(function() {
-  if (emotionsArray[default_values.HAPPY_INDEX]) {
-    emotionsArray[default_values.HAPPY_INDEX] = false;
-    document.getElementById("happy-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.HAPPY_INDEX] = true;
-    document.getElementById("happy-button").style.backgroundColor='#D7DA3F';
-  }
+  inverse(default_values.HAPPY_INDEX, "happy-button","#D7DA3F");
+  // if (emotionsArray[default_values.HAPPY_INDEX]) {
+  //   emotionsArray[default_values.HAPPY_INDEX] = false;
+  //   document.getElementById("happy-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.HAPPY_INDEX] = true;
+  //   document.getElementById("happy-button").style.backgroundColor='#D7DA3F';
+  // }
 });
 
 $("#sad-button").click(function() {
-  if (emotionsArray[default_values.SAD_INDEX]) {
-    emotionsArray[default_values.SAD_INDEX] = false;
-    document.getElementById("sad-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.SAD_INDEX] = true;
-    document.getElementById("sad-button").style.backgroundColor='#0369C2';
-  }
+  inverse(default_values.SAD_INDEX,"sad-button","#0369C2");
+  // if (emotionsArray[default_values.SAD_INDEX]) {
+  //   emotionsArray[default_values.SAD_INDEX] = false;
+  //   document.getElementById("sad-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.SAD_INDEX] = true;
+  //   document.getElementById("sad-button").style.backgroundColor='#0369C2';
+  // }
 });
 
 $("#frustrated-button").click(function() {
-  if (emotionsArray[default_values.FRUSTRATED_INDEX]) {
-    emotionsArray[default_values.FRUSTRATED_INDEX] = false;
-    document.getElementById("frustrated-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.FRUSTRATED_INDEX] = true;
-    document.getElementById("frustrated-button").style.backgroundColor='#F2393D';
-  }
+  inverse(default_values.FRUSTRATED_INDEX,"frustrated-button","#F2393D");
+  // if (emotionsArray[default_values.FRUSTRATED_INDEX]) {
+  //   emotionsArray[default_values.FRUSTRATED_INDEX] = false;
+  //   document.getElementById("frustrated-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.FRUSTRATED_INDEX] = true;
+  //   document.getElementById("frustrated-button").style.backgroundColor='#F2393D';
+  // }
 });
 
 $("#tired-button").click(function() {
-  if (emotionsArray[default_values.TIRED_INDEX]) {
-    emotionsArray[default_values.TIRED_INDEX] = false;
-    document.getElementById("tired-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.TIRED_INDEX] = true;
-    document.getElementById("tired-button").style.backgroundColor='#8680C6';
-  }
+  inverse(default_values.TIRED_INDEX,"tired-button","#8680C6");
+  // if (emotionsArray[default_values.TIRED_INDEX]) {
+  //   emotionsArray[default_values.TIRED_INDEX] = false;
+  //   document.getElementById("tired-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.TIRED_INDEX] = true;
+  //   document.getElementById("tired-button").style.backgroundColor='#8680C6';
+  // }
 });
 
 $("#studious-button").click(function() {
-  if (emotionsArray[default_values.STUDIOUS_INDEX]) {
-    emotionsArray[default_values.STUDIOUS_INDEX] = false;
-    document.getElementById("studious-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.STUDIOUS_INDEX] = true;
-    document.getElementById("studious-button").style.backgroundColor='#50B8B7';
-  }
+  inverse(default_values.STUDIOUS_INDEX,"studious-button","#50B8B7");
+  // if (emotionsArray[default_values.STUDIOUS_INDEX]) {
+  //   emotionsArray[default_values.STUDIOUS_INDEX] = false;
+  //   document.getElementById("studious-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.STUDIOUS_INDEX] = true;
+  //   document.getElementById("studious-button").style.backgroundColor='#50B8B7';
+  // }
 });
 
 $("#energetic-button").click(function() {
-  if (emotionsArray[default_values.ENERGETIC_INDEX]) {
-    emotionsArray[default_values.ENERGETIC_INDEX] = false;
-    document.getElementById("energetic-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.ENERGETIC_INDEX] = true;
-    document.getElementById("energetic-button").style.backgroundColor='#FF76B4';
-  }
+  inverse(default_values.ENERGETIC_INDEX,"energetic-button","#FF76B4");
+  // if (emotionsArray[default_values.ENERGETIC_INDEX]) {
+  //   emotionsArray[default_values.ENERGETIC_INDEX] = false;
+  //   document.getElementById("energetic-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.ENERGETIC_INDEX] = true;
+  //   document.getElementById("energetic-button").style.backgroundColor='#FF76B4';
+  // }
 });
 
 $("#anxious-button").click(function() {
-  if (emotionsArray[default_values.ANXIOUS_INDEX]) {
-    emotionsArray[default_values.ANXIOUS_INDEX] = false;
-    document.getElementById("anxious-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.ANXIOUS_INDEX] = true;
-    document.getElementById("anxious-button").style.backgroundColor='#FF7F00';
-  }
+  inverse(default_values.ANXIOUS_INDEX,"anxious-button","#FF7F00");
+  // if (emotionsArray[default_values.ANXIOUS_INDEX]) {
+  //   emotionsArray[default_values.ANXIOUS_INDEX] = false;
+  //   document.getElementById("anxious-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.ANXIOUS_INDEX] = true;
+  //   document.getElementById("anxious-button").style.backgroundColor='#FF7F00';
+  // }
 });
 
 $("#nauseous-button").click(function() {
-  if (emotionsArray[default_values.NAUSEOUS_INDEX]) {
-    emotionsArray[default_values.NAUSEOUS_INDEX] = false;
-    document.getElementById("nauseous-button").style.backgroundColor='#4dabf7';
-  }
-  else {
-    emotionsArray[default_values.NAUSEOUS_INDEX] = true;
-    document.getElementById("nauseous-button").style.backgroundColor='#4DBE46';
-  }
+  inverse(default_values.NAUSEOUS_INDEX,"nauseous-button","#4DBE46");
+  // if (emotionsArray[default_values.NAUSEOUS_INDEX]) {
+  //   emotionsArray[default_values.NAUSEOUS_INDEX] = false;
+  //   document.getElementById("nauseous-button").style.backgroundColor='#4dabf7';
+  // }
+  // else {
+  //   emotionsArray[default_values.NAUSEOUS_INDEX] = true;
+  //   document.getElementById("nauseous-button").style.backgroundColor='#4DBE46';
+  // }
 });
 
 // Map of moods to indices (to align with emotionsArray order)
--- recommendations/js/recommendations.js
+++ recommendations/js/recommendations.js
@@ -167,9 +167,11 @@ const recipeData = {
 
   */
 
+  //below group of code has been slightly modified to now work with the array of booleans rather than strings
   const selectedMoods = JSON.parse(localStorage.getItem('selectedMoods')) || [];
   const outputDiv = document.getElementById('output');
   
+  //bandaid code added so that changing to an arr of booleans works, code to be deleted later
   const moods = ["happy", "sad", "frustrated", "tired", "studious", "energetic", "anxious", "nauseous"];
   let temp = 0;
 
@@ -187,7 +189,7 @@ const recipeData = {
       });
     }
 
-    
+    //part of the band-aid code
     temp++;
   });
   
