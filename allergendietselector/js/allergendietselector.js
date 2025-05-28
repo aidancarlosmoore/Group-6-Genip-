a6bd744a0db446a46f3b64e4e2341891ec51af92
lily-diamond <brielle.laplante22@gmail.com>
2025-05-28 10:37:10 -0700

    Polishing and cleaning up
    cleaned up allergen-diet a lot, fixed some centering inconsistencies

--- allergendietselector/js/allergendietselector.js
+++ allergendietselector/js/allergendietselector.js
@@ -2,10 +2,16 @@
 import * as default_value from "../../js/site.js";
 //console.log(default_value.PEANUT_INDEX);
 
+let on_allergen_tab = true;
+let on_diet_tab = false;
+
+
 //initialize variable
 let selected_allergens_diet;
 
 //funct to be ran once user clicks the submit button
+//run with allergenDietSubmit()
+//returns nothing but saves selected allergens and diets to storage.
 let allergenDietSubmit = function(){
     //console.log("click");
 
@@ -35,8 +41,46 @@ let allergenDietSubmit = function(){
 
 };
 
-//when button is clicked
+
+//added tab switching functionality to the allergen/diet selector
+//call with tabSwitcher(boolean) the boolean describes wether or not the button pushed was the allergen button
+//returns nothing, but hides and shows appropriate checkboxes
+let tabSwitcher = function(is_allergen_button)
+{
+    //console.log("clicked tab button with values alltab: "+on_allergen_tab+" and diettab: "+on_diet_tab);
+    if(on_allergen_tab && !is_allergen_button)
+    {
+        $(".allergen-checkboxes").hide();
+        $(".diet-checkboxes").show();
+        on_allergen_tab = false;
+        on_diet_tab = true;
+    }
+    else if(on_diet_tab && is_allergen_button)
+    {
+        $(".diet-checkboxes").hide();
+        $(".allergen-checkboxes").show();
+        on_allergen_tab = true;
+        on_diet_tab = false;
+    }
+    else
+    {
+        //console.log("clicked same tab");
+    }
+    
+};
+
+//initialize diet as being hidden
+$(".diet-checkboxes").hide();
+
+//when submit button is clicked
 $(".allergen-diet-button").click(allergenDietSubmit);
 
+//when either tab switcher button is clicked
+$(".allergen-tab-button").click(function(){
+    tabSwitcher(true);
+});
+$(".diet-tab-button").click(function(){
+    tabSwitcher(false);
+});
 
 
--- moodselector/js/moodselector.js
+++ moodselector/js/moodselector.js
@@ -1,39 +1,29 @@
 //imports the global constant varibales
-import * as default_values from "../../js/site.js";
+import * as default_value from "../../js/site.js";
 
 // Some code here has been commented out and reimplemented so that it works with boolean array output
-//
-//
-//
 
 // Makes it so that when you click on the "Return to Title Screen" button, you go to that page
 $("#title-screen").click(function() {
   window.location.href = "index.html";
 });
 
-// Initializing all the Boolean variables of the user's emotions
-/*
-let happy = false;
-let sad = false;
-let frustrated = false;
-let tired = false;
-let studious = false;
-let energetic = false;
-let anxious = false;
-let nauseous = false;
-*/
+//stores the color we want the unclicked button to be
+const UNCLICKED_BUTTON_COLOR_HEX = "#4dabf7";
 
 // Initializing an array with each of the emotion Booleans so that they can be easily accessed as a group
 let emotionsArray = [false, false, false, false, false, false, false, false];
 
-//new function implemented to replace redundant code
+//new function implemented to replace redundant code, inverses color and boolean in emotionsArray
+//call with inverse(int index, string id, string color) where index is in range [0,8], id is a valid id of a button in coresponding index.html, and color is a string in hexadecimal format
+//returns nothing, but flips color and state of emotion value in the array.
 let inverse = function(index, id, color){
   //console.log("clicked on " + id + " in state: " + emotionsArray[index])
 
   //flips color
   if(emotionsArray[index])
   {
-    document.getElementById(id).style.backgroundColor="#4dabf7";
+    document.getElementById(id).style.backgroundColor=UNCLICKED_BUTTON_COLOR_HEX;
   }
   else
   {
@@ -42,40 +32,40 @@ let inverse = function(index, id, color){
 
   //flips value of what was stored
   emotionsArray[index] = !emotionsArray[index];
-  ;
+  
 };
 
 // Creates an event listener that updates the emotion Booleans in the array based on user clicks, as well as the button's color by calling inverse()
 $("#happy-button").click(function() {
-  inverse(default_values.HAPPY_INDEX, "happy-button","#D7DA3F");
+  inverse(default_value.HAPPY_INDEX, "happy-button","#D7DA3F");
 });
 
 $("#sad-button").click(function() {
-  inverse(default_values.SAD_INDEX,"sad-button","#0369C2");
+  inverse(default_value.SAD_INDEX,"sad-button","#0369C2");
 });
 
 $("#frustrated-button").click(function() {
-  inverse(default_values.FRUSTRATED_INDEX,"frustrated-button","#F2393D");
+  inverse(default_value.FRUSTRATED_INDEX,"frustrated-button","#F2393D");
 });
 
 $("#tired-button").click(function() {
-  inverse(default_values.TIRED_INDEX,"tired-button","#8680C6");
+  inverse(default_value.TIRED_INDEX,"tired-button","#8680C6");
 });
 
 $("#studious-button").click(function() {
-  inverse(default_values.STUDIOUS_INDEX,"studious-button","#50B8B7");
+  inverse(default_value.STUDIOUS_INDEX,"studious-button","#50B8B7");
 });
 
 $("#energetic-button").click(function() {
-  inverse(default_values.ENERGETIC_INDEX,"energetic-button","#FF76B4");
+  inverse(default_value.ENERGETIC_INDEX,"energetic-button","#FF76B4");
 });
 
 $("#anxious-button").click(function() {
-  inverse(default_values.ANXIOUS_INDEX,"anxious-button","#FF7F00");
+  inverse(default_value.ANXIOUS_INDEX,"anxious-button","#FF7F00");
 });
 
 $("#nauseous-button").click(function() {
-  inverse(default_values.NAUSEOUS_INDEX,"nauseous-button","#4DBE46");
+  inverse(default_value.NAUSEOUS_INDEX,"nauseous-button","#4DBE46");
 });
 
 // Map of moods to indices (to align with emotionsArray order)
